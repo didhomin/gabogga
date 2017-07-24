3b8b8d19370df264ad89333bf81bfedb1ce04513
권성준 <kwen5600@naver.com>
2017-07-24 20:21:53 +0900

    commit

--- src/com/gbg/host/controller/HostController.java
+++ src/com/gbg/host/controller/HostController.java
@@ -1,5 +1,21 @@
 package com.gbg.host.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/host")
 public class HostController {
+
+//		@Autowired
+//		private HostService hostrService;
+		
+		@RequestMapping(value="/register.gbg", method=RequestMethod.GET)
+		public String register(){
+			System.out.println("들어왓니");
+			return "/page/host/first";
+		}
 
 }
